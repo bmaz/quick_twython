15bb3964e947dab1b46a76083eec45cc584cc70c
Béatrice Mazoyer <beatrice.mazoyer@gmail.com>
2016-07-06 17:51:41 +0200

    setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='bmazoyer',
+      version='1.0',
+      py_modules=['twython'],
+      url='http://github.com/bmaz/quick_twython',
+      classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3'
+        ]
+      )
