79531e5e28b20ee14745d8c24e274371a2fe9c8a
Béatrice Mazoyer <beatrice.mazoyer@gmail.com>
2016-07-12 17:45:48 +0200

    threads

--- quick_twython/restApi.py
+++ quick_twython/restApi.py
@@ -11,14 +11,17 @@ class TwythonWrapper(Twython):
     def __init__(self, app_key, app_secret, oauth_token, oauth_token_secret, filedir):
         self.filedir = filedir
         Twython.__init__(self, app_key, app_secret, oauth_token, oauth_token_secret)
+        self.reset_time = float(
+            self.get_application_rate_limit_status()["resources"]["search"]["/search/tweets"]["reset"]
+        )
 
-    def on_rate_limit_error(self, error):
-        remainder = float(self.get_lastfunction_header(header='x-rate-limit-reset')) - time.time()
-        print("RateLimitError :",error)
-        minutes, seconds = divmod(remainder, 60)
-        print("Sleeping for " + str(round(minutes)) + "\'" + str(round(seconds)) + "\'\'")
-        if(remainder >0):
-            time.sleep(remainder)
+    def on_rate_limit_error(self, method):
+        if method == "search":
+            reset_time = float(
+                self.get_application_rate_limit_status()["resources"]["search"]["/search/tweets"]["reset"]
+            )
+        return reset_time
+
 
     def on_error(self, error):
         print("Some other error occured, taking a break for half a minute: " + str(error))
@@ -28,6 +31,7 @@ class TwythonWrapper(Twython):
     def storeTweets(self, tweets, name=""):
         """
         Store tweets in a file called "name_nb-of-tweets_date-and-time.json"
+        Tweets are separated by "\n"
         :param tweets: a list of tweets to store
         :param name: a name for the store file
         """
@@ -44,13 +48,12 @@ class TwythonWrapper(Twython):
 
 
 
-    def past_search(self, query, since_id=None, until=None):
+    def past_search(self, query, next_max_id=None, since_id=None, until=None):
         """
         Returns a collection of relevant Tweets matching a specified query.
         Docs: https://dev.twitter.com/docs/api/1.1/get/search/tweets
         """
         tweets = []
-        next_max_id = 0
         print("Start searching for query " + query)
 
         for i in range(0,MAX_ATTEMPTS):
@@ -69,11 +72,8 @@ class TwythonWrapper(Twython):
             #----------------------------------------------------------------#
             try :
                 # STEP 1: Query Twitter
-                if(0 == i):
-                    results = self.search(q=query, lang=LANG, count='100', since_id=since_id, until=until)
-                else:
-                    # After the first call we should have max_id from result of previous call. Pass it in query.
-                    results = self.search(q=query, lang =LANG, max_id=next_max_id, count='100', since_id=since_id, until=until)
+                # After the first call we should have max_id from result of previous call. Pass it in query.
+                results = self.search(q=query, lang =LANG, max_id=next_max_id, count='100', since_id=since_id, until=until)
 
                 # STEP 2: Save the returned tweets
                 for result in results['statuses']:
@@ -86,14 +86,13 @@ class TwythonWrapper(Twython):
                     next_results_url_params = results['search_metadata']['next_results']
                     next_max_id = next_results_url_params.split('max_id=')[1].split('&')[0]
 
-                except Exception as err:
-                    print("Error : ", err)
+                except Exception:
+                    print("OVER !!!")
                     # No more next pages
                     break
 
             except TwythonRateLimitError as error:
-                self.on_rate_limit_error(error)
-                continue
+                break
             except TwythonError as error:
                 self.on_error(error)
                 continue
@@ -103,5 +102,5 @@ class TwythonWrapper(Twython):
 if __name__ == "__main__":
     filedir = "/home/bmazoyer/Documents/TwitterSea/Tests/"
     twitter = TwythonWrapper(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET,filedir)
-    tweets = twitter.past_search("ceci est un test")
+    tweets = twitter.past_search("#JeSuisPapillon")
     twitter.storeTweets(tweets, "test")
--- quick_twython/threads.py
+++ quick_twython/threads.py
@@ -1,7 +1,48 @@
 import threading
+import logging
 from restApi import *
+from config import *
 
-    filedir = "/home/bmazoyer/Documents/TwitterSea/Tests/"
-    twitter = TwythonWrapper(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET,filedir)
-    tweets = twitter.past_search("#JeSuisPapillon")
-    twitter.storeTweets(tweets, "test")
+filedir = "/home/bmazoyer/Documents/TwitterSea/Tests/"
+
+# logging.basicConfig(level=logging.DEBUG,format='[%(levelname)s] (%(threadName)-10s) %(message)s')
+
+query = "Gaza"
+nom_du_fichier = "Gaza"
+tweets = []
+lock = threading.RLock()
+
+class SearchThread(threading.Thread):
+    def __init__(self, threadNum):
+        threading.Thread.__init__(self)
+        key = ACCESS[threadNum]
+        self.threadNum = threadNum
+        self.twitter =  TwythonWrapper(
+            key["consumer_key"],
+            key["consumer_secret"],
+            key["oauth_token"],
+            key["oauth_token_secret"],
+            filedir)
+
+    def run(self):
+        with lock:
+            print("et c'est parti pour le thread ", self.threadNum)
+            if tweets == []:
+                result = self.twitter.past_search(query)
+            else:
+                result = self.twitter.past_search(query, min(t["id"] for t in tweets))
+            tweets.extend(result)
+        self.twitter.storeTweets(result, nom_du_fichier)
+
+while True:
+    for i in range(len(ACCESS)):
+        t = SearchThread(i)
+        t.start()
+        if i == 0:
+            reset_time = t.twitter.on_rate_limit_error("search")
+
+    if reset_time - time.time()>0:
+        print("prochain départ dans :", reset_time - time.time())
+        time.sleep(reset_time - time.time())
+    else:
+        print(reset_time - time.time())
